aca1b6cf3b79b67f4b9f4d216a2fa51c80e8b959
bohun <slawkd2002@gmail.com>
2024-01-24 19:33:04 +0900

    Feat : 마우스 이동
    마우스 좌클릭으로 좌측이동,
    마우스 우클릭으로 우측이동

--- Assets/Bohun/Scripts/Controller/CharacterController.cs
+++ Assets/Bohun/Scripts/Controller/CharacterController.cs
@@ -7,8 +7,10 @@ public class CharacterController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
 
+
     public void CallMoveEvent(Vector2 dir)
     {
         OnMoveEvent?.Invoke(dir);
     }
+
 }
--- Assets/Bohun/Scripts/Controller/PlayerInputController.cs
+++ Assets/Bohun/Scripts/Controller/PlayerInputController.cs
@@ -8,11 +8,30 @@ public class PlayerInputController : CharacterController
     public void OnMove(InputValue value)
     {
         Vector2 dirVec = value.Get<Vector2>().normalized;
+        Debug.Log(dirVec);
         Debug.Log("Move");
         CallMoveEvent(dirVec);
     }
-    public void OnRightClick(InputValue value)
+    public void OnRightClick(InputValue inputValue)
     {
-
+        Debug.Log(inputValue);
+        Vector2 dirVec = Vector2.right;
+       
+        if (!inputValue.isPressed)
+        {
+            dirVec = Vector2.zero;
+        }
+        CallMoveEvent(dirVec);
+    }
+    public void OnLeftClick(InputValue inputValue)
+    {
+        Debug.Log(inputValue);
+        Vector2 dirVec = Vector2.left;
+        
+        if (!inputValue.isPressed)
+        {
+            dirVec = Vector2.zero;
+        }
+        CallMoveEvent(dirVec);
     }
 }
