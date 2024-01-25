9916fa9af665e6a3759c7aa92834a17b39baf0d1
minsooBak <minsoo9716@gmail.com>
2024-01-25 14:27:17 +0900

    Feat : Pentagon Change
    6각형에서 5각형으로 바뀌는 테스트코드

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -5,21 +5,36 @@ public class ObjectManager : MonoBehaviour
 {
     //TODO : 오브젝트클래스에 생성과 생성 수 넘겨주기
     public GameObject map;
+    public Object[] objects;
 
+    private Animator _animator;
+    //private int phaseLevel = 1;
+    [SerializeField] private float _dealy = 5f;
     private void Awake()
     {
-        Instantiate(map);
+        Instantiate(map, transform);
+        objects = GetComponentsInChildren<Object>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        int r = 0;
+        int s = Random.Range(5, 20);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach(Object obj in objects)
+            {
+                obj.CreateWall(s, ++r);
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            _animator.SetTrigger("1_Disabled");
+        }else if(Input.GetKeyDown(KeyCode.D))
+        {
+            _animator.SetTrigger("1_Active");
+        }
     }
 }
