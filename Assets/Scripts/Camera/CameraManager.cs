d6e9f6825dc2b107886f948e9a1a3e5cd2deff13
minsooBak <minsoo9716@gmail.com>
2024-01-29 10:52:10 +0900

    Refactor : CameraManager
    메인카메라 설정 및 초기 앵글값 수정,카메라 회전 x, y축 고정했습니다

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public GameObject mainCamera;
+    public Camera mainCamera;
     public static CameraManager I;
 
     Transform _transform;
 
 
     [Header("Camera Settings")]
-    [SerializeField] [Range(1, 90)] float tiltAngle = 30f;
+    [SerializeField] [Range(1, 90)] float tiltAngle = 1f;
     [SerializeField] [Range(0f, 10f)] float rotationSpeed = 1.0f;
     [SerializeField] [Range(1f, 30f)] float zoomAmount = 10f;
     [SerializeField] bool isClockwise = true; // true = 시계방향, false = 반시계방향
@@ -25,6 +25,8 @@ public class CameraManager : MonoBehaviour
 
     private void Awake()
     {
+        if (mainCamera == null)
+            mainCamera = Camera.main;
         _transform = mainCamera.GetComponent<Transform>();
         I = this;
     }
@@ -55,6 +57,7 @@ public class CameraManager : MonoBehaviour
         cameraX = Mathf.Sin(tiltAngle * Mathf.Deg2Rad) * Mathf.Cos(rotationAngle * Mathf.Deg2Rad) * zoomAmount;
         cameraY = Mathf.Sin(tiltAngle * Mathf.Deg2Rad) * Mathf.Sin(rotationAngle * Mathf.Deg2Rad) * zoomAmount;
         cameraZ = Mathf.Cos(tiltAngle * Mathf.Deg2Rad) * zoomAmount * (-1);
+        mainCamera.orthographicSize = zoomAmount;
         headDirecton = new Vector3(0f, 0f, cameraZ);
     }
 
@@ -66,6 +69,7 @@ public class CameraManager : MonoBehaviour
     private void UpdateCameraRotation()
     {
         _transform.rotation = Quaternion.LookRotation(worldOrigin - _transform.position, headDirecton);
+        _transform.rotation = new Quaternion(0, 0, _transform.rotation.z, _transform.rotation.w);
     }
 
     private int ReturnDirection(bool clockwise)
