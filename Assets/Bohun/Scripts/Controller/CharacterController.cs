abbf105edd1bf5048fa1745a0e1a957db2710039
bohun <slawkd2002@gmail.com>
2024-01-29 16:45:04 +0900

    Feat : 힐 포션 추가

--- Assets/Bohun/Scripts/Controller/CharacterController.cs
+++ Assets/Bohun/Scripts/Controller/CharacterController.cs
@@ -5,10 +5,8 @@ public class CharacterController : MonoBehaviour
 {
     public event Action<Vector2> OnMoveEvent;
 
-
     public void CallMoveEvent(Vector2 dir)
     {
         OnMoveEvent?.Invoke(dir);
     }
-
 }
