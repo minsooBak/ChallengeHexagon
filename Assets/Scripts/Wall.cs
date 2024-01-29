7e3c65ceb78c949cda2bc7a0b5b4b80db3ce5c5c
minsooBak <minsoo9716@gmail.com>
2024-01-29 11:18:45 +0900

    Feat : Wall Script
    이벤트 타입 별 색상변경 및 초기화 수정

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class Wall : MonoBehaviour
 {
-    [SerializeField]private float _speed = 5f;
+    [SerializeField] private float _speed = 5f;
     private int _damage = 0;
-    int _dataIndex = -1;
+    private int _dataIndex = -1;
+
+    private SpriteRenderer _renderer;
+    private EventManager _eventManager;
+
+    [SerializeField] private Material _defultMaterial;
+    [SerializeField] private Material[] _materials;
 
     void Update()
     {
         CheckUpdate();
         if (transform.localPosition.y >= 0.56f)
         {
+            if (_dataIndex != -1)
+            {
+                GameManager.I.EventManager.DeleteData(_dataIndex);
+                _dataIndex = -1;
+            }
             gameObject.SetActive(false);
         }
         transform.position = Vector3.MoveTowards(transform.position, Vector3.zero, _speed * Time.deltaTime);
@@ -22,21 +33,38 @@ public class Wall : MonoBehaviour
         EventMain _event = collision.GetComponent<EventMain>();
         _event.OnWallEvent(_dataIndex, -_damage);
         _dataIndex = -1;
+        _renderer.material = _defultMaterial;
         gameObject.SetActive(false);
     }
 
     public void Setting(int layer)
     {
-        GetComponent<SpriteRenderer>().sortingOrder = layer;
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.material = _defultMaterial;
+        _eventManager = GameManager.I.EventManager;
+        _renderer.sortingOrder = layer;
     }
 
     public void SettingData(int index)
     {
-        _dataIndex = index;
+        //TODO : Type에 따른 메테리얼 변경
+        if (index == -1)
+        {
+            _renderer.material = _defultMaterial;
+        }
+        else
+        {
+            _dataIndex = index;
+            _renderer.material = _materials[_eventManager.GetType(index)];
+        }
     }
 
     public void Init(int speed, int damage)
     {
+        if(_dataIndex == -1)
+        {
+            _renderer.material = _defultMaterial;
+        }
         _speed = speed;
         _damage = damage;
         transform.localPosition = new Vector3(0, -0.4f, 0);
