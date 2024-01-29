576041a2c7010c808591133d070e78b9160d2811
najdorf <70437537+cn7249@users.noreply.github.com>
2024-01-29 17:57:37 +0900

    Feat : Added animation of HP hexagon which perfectly fits on current BPM
    스테이지 BGM의 BPM(박자)에 완벽히 들어맞는 애니메이션을 HP hexagon에 추가했습니다.

--- Assets/Scripts/UI/InGame/HpHexagon.cs
+++ Assets/Scripts/UI/InGame/HpHexagon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class HpHexagon : MonoBehaviour
@@ -10,21 +11,65 @@ public class HpHexagon : MonoBehaviour
 
     private SpriteRenderer _sprite;
 
+    private Vector3 minScale;
+    private Vector3 originScale;
+    private float chunk;
+    private float timer;
+    private float zoomSpeed;
+    private int bpm;
+    private int count;
+
+
+
     private void Awake()
     {
         _sprite = hexagon.GetComponent<SpriteRenderer>();
         hp = 100;
+        chunk = 0f;
+        bpm = 110;
+        timer = 0f;
+        count = 1;
+        zoomSpeed = 1.0f;
+    }
+
+    private void Start()
+    {
+
     }
 
     private void Update()
     {
-        _sprite.transform.localScale = new Vector3(hp / 100 * 0.9f, hp / 100 * 0.9f, 1);
+        SetScale();
+        CheckBPM(bpm);
     }
 
-    private void MakeSpriteDance(int bpm)
+    private void SetScale()
     {
-        float beatTime = 60f / bpm * Time.deltaTime;
-
-
+        originScale = new Vector3(hp / 100 * 0.9f, hp / 100 * 0.9f, 1);
+        minScale = originScale * 0.9f;
     }
+
+    private void CheckBPM(int bpm)
+    {
+        chunk = 60f / bpm;
+        timer += Time.deltaTime;
+
+        if (timer >= chunk * count)
+        {
+            MakeScaleMin();
+            Invoke("MakeScaleOrigin", chunk / 3);
+            count++;
+        }
+    }
+
+    private void MakeScaleMin()
+    {
+        _sprite.transform.localScale = Vector3.Lerp(originScale, minScale, Time.deltaTime * zoomSpeed);
+    }
+
+    private void MakeScaleOrigin()
+    {
+        _sprite.transform.localScale = Vector3.Lerp(minScale, originScale, Time.deltaTime * zoomSpeed);
+    }
+
 }
